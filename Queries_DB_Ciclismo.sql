b1cafdeefc529f064ee13adcf4817edc4bdaf7a6
Poli22 <lucapili01@gmail.com>
2022-06-02 23:18:08 +0200

    commit 6

--- Queries_DB_Ciclismo.sql
+++ Queries_DB_Ciclismo.sql
@@ -1,10 +1,10 @@
-DECLARE cod_ciclista as CHAR(5);
-SET cod_ciclista = 'C0001';
-DECLARE cod_tappa as CHAR(5);
-SET cod_tappa = 'T0001';
+DECLARE cod_ciclista as SMALLINT;
+SET cod_ciclista = 1;
+DECLARE cod_tappa as SMALLINT;
+SET cod_tappa = 1;
 
-SELECT Nome, Cognome, NomeS, CodTappa, Edizione, Posizione
+SELECT Nome, Cognome, NomeS, CodT, Edizione, Posizione
 FROM CICLISTA C, CLASSIFICA_INDIVIDUALE CI, SQUADRA S
 WHERE C.CodC = CI.CodC AND C.CodS = S.CodS
-AND C.CodC = cod_ciclista AND CI.CodTappa = cod_tappa
+AND C.CodC = cod_ciclista AND CI.CodT = cod_tappa
 ORDER BY Edizione;
--- app.py
+++ app.py
@@ -7,32 +7,143 @@ app = Flask(__name__)
 
 
 @app.route("/")
+def home():
+    return render_template("Home.html")
+
+
+@app.route("/campionato_ciclistico")
 def ind():
     dialect = "mysql"
-    username="root"
-    password=""
-    host="127.0.0.1:3306"
-    dbname = "Campionato_Ciclistico"
-    #Connection object creation
-    engine = create_engine("%s://%s:%s@%s/%s"%(dialect,username,password,host,dbname))
+    username = "root"
+    password = ""
+    host = "127.0.0.1:3306"
+    dbname = "campionato_ciclistico"
+    engine = create_engine("%s://%s:%s@%s/%s" % (dialect, username, password, host, dbname))
+
+    action = request.args.get('action')
+
+    if action == "Ricerca nel database":
+
+        try:
+            con = engine.connect()
+
+            query = "SELECT CodC, Cognome, Nome\
+                    FROM CICLISTA\
+                    ORDER BY Cognome, Nome"
+
+            ciclisti = con.execute(query)
+
+            con.close()
+
+            return render_template('Form_Query.html', ciclisti=ciclisti)
+        except SQLAlchemyError as e:
+
+            error = str(e.__dict__['orig'])
+            return render_template('KO.html', error_message=error)
+
+    elif action == "Aggiornamento classifica":
+
+        try:
+            con = engine.connect()
+
+            query = "SELECT CodC, Cognome, Nome\
+                            FROM CICLISTA\
+                            ORDER BY Cognome, Nome"
+
+            ciclisti = con.execute(query)
+
+            query = "SELECT CodT, Edizione\
+                    FROM TAPPA\
+                    ORDER BY CodT, Edizione"
+
+            tappe = con.execute(query)
+
+            con.close()
+
+            return render_template('Form_Tappa_Ins.html', ciclisti=ciclisti, tappe=tappe)
+        except SQLAlchemyError as e:
+
+            error = str(e.__dict__['orig'])
+            return render_template('KO.html', error_message=error)
+
+    elif action == "Inserimento ciclista":
+        try:
+            con = engine.connect()
+
+            query = "SELECT CodS, NomeS\
+                    FROM SQUADRA\
+                    ORDER BY CodS, NomeS"
+
+            squadre = con.execute(query)
+
+            con.close()
+
+            return render_template("Form_Ciclista_Ins.html", squadre=squadre)
+        except SQLAlchemyError as e:
+
+            error = str(e.__dict__['orig'])
+            return render_template('KO.html', error_message=error)
+    else:
+        return render_template('KO.html', error_message="Azione non valida")
+
+
+@app.route("/result")
+def read_form_query():
+    dialect = "mysql"
+    username = "root"
+    password = ""
+    host = "127.0.0.1:3306"
+    dbname = "campionato_ciclistico"
+    engine = create_engine("%s://%s:%s@%s/%s" % (dialect, username, password, host, dbname))
 
     try:
         con = engine.connect()
 
-        #QUERY SQL
-        query="SELECT CodC, Cognome, Nome\
-               FROM CICLISTA\
-               ORDER BY Cognome, Nome"
+        ciclista = int(request.args.get('cod_ciclista'))
+        tappa = int(request.args.get('cod_tappa'))
 
-        cyclists = con.execute(query)
+        if tappa == "":
+            return render_template('Risultato.html', error_message="Valori inseriti non validi")
+
+        query = "SELECT Nome, Cognome, NomeS, CodT, Edizione, Posizione\
+                FROM CICLISTA C, CLASSIFICA_INDIVIDUALE CI, SQUADRA S\
+                WHERE C.CodC = CI.CodC AND C.CodS = S.CodS\
+                AND C.CodC = '%d' AND CI.CodT = '%d'\
+                ORDER BY Edizione" % (ciclista, tappa)
+
+        res = con.execute(query)
 
         con.close()
 
-        return render_template('Form_Query.html', CYCLISTS = cyclists)
+        return render_template('Risultato.html', res=res)
     except SQLAlchemyError as e:
 
         error = str(e.__dict__['orig'])
-        return render_template('Ins_Error.html', error_message=error)
+        return render_template('KO.html', error_message=error)
 
 
-app.run(debug=True, port=3306)
+@app.route("/aggiornamento_classifica")
+def aggiornamento_classifica():
+    dialect = "mysql"
+    username = "root"
+    password = ""
+    host = "127.0.0.1:3306"
+    dbname = "campionato_ciclistico"
+    engine = create_engine("%s://%s:%s@%s/%s" % (dialect, username, password, host, dbname))
+
+    try:
+        con = engine.connect()
+
+        ciclista = int(request.args.get("cod_ciclista"))
+        tappa = [int(s) for s in request.args.get("cod_ed_tappa") if s.isdigit()]
+        #qua devo inserire la query tappa[0] = codT e tappa[1] = edizione
+        con.close()
+
+        return render_template('OK.html')
+    except SQLAlchemyError as e:
+
+        error = str(e.__dict__['orig'])
+        return render_template('KO.html', error_message=error)
+
+
+app.run(debug=True, port=8080)
